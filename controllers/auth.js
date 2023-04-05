21d350ce03e48caa645ff5f816107ed22c7fec63
gejaga12 <gejaga12@hotmail.com>
2023-04-05 11:29:44 -0300

    validaciones con express-validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,18 @@
 const { response } = require('express');
+const { validationResult } = require('express-validator');
 
 const crearUsuario = (req, res = response) => {
 
     const { name, email, password } = req.body;
 
-    if (name.length < 5) {
-        return res.status(400).json({
-            ok: false,
-            msg: "El nombre debe tener más de 5 caracteres"
-        })
-    }
+    // MANEJO DE ERRORES
+    const errors = validationResult( req );
+    if (!errors.isEmpty()) return res.status(400).json({
+        ok: false,
+        errors: errors.mapped()
+    })
 
-    res.json({
+    res.status(201).json({
         ok: true,
         msg: 'registro',
         name,
@@ -24,7 +25,13 @@ const loginUsuario = (req, res = response) => {
 
     const { email, password } = req.body;
 
-    res.json({
+    const errors = validationResult( req )
+    if ( !errors.isEmpty() ) return res.status(400).json({
+        ok: false,
+        errors: errors.mapped()
+    }) 
+
+    res.status(200).json({
         ok: true,
         msg: 'login',
         email,
--- routes/auth.js
+++ routes/auth.js
@@ -4,15 +4,30 @@
 */
 
 const { Router } = require('express');
+const { check } = require('express-validator')
 const router = Router();
 
 const { crearUsuario } = require('../controllers/auth');
 const { loginUsuario } = require('../controllers/auth')
 const { revalidarToken } = require('../controllers/auth')
 
-router.post('/new', crearUsuario)
+router.post(
+    '/new',
+    [   //middlewares
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 })
+    ],
+    crearUsuario);
 
-router.post('/', loginUsuario)
+router.post(
+    '/',
+    [
+        check('email', 'El email es obligatorio').isEmail(),
+        check('password', 'El password debe de ser de 6 caracteres').isLength({ min: 6 })
+    ],
+    loginUsuario);
+
 
 router.get('/renew', revalidarToken)
 
