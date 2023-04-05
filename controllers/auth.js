811aa415df94dfd56397b581a2fd448b631011fc
gejaga12 <gejaga12@hotmail.com>
2023-04-05 16:06:34 -0300

    contraseña hash con bcrypt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,51 @@
 const { response } = require('express');
+const bcrypt = require('bcryptjs')
+const Usuario = require('../models/Usuario')
 
-const crearUsuario = (req, res = response) => {
 
-    const { name, email, password } = req.body;
 
-    res.status(201).json({
-        ok: true,
-        msg: 'registro',
-        name,
-        email,
-        password,
-    });
+const crearUsuario = async (req, res = response) => {
+
+    const { email, password } = req.body;
+
+    try {
+
+        let usuario = await Usuario.findOne({ email })
+
+        if (usuario) {
+            return res.status(400).json({
+                ok: false,
+                msg: "Un usuario ya existe con ese correo"
+            });
+        }
+
+        usuario = new Usuario(req.body);
+
+        // ENCRIPTAR CONTRASEÑA
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync( password, salt );
+
+        await usuario.save();
+
+        res.status(201).json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name,
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: "Por favor hable con el Administrador"
+        })
+    }
 }
 
 const loginUsuario = (req, res = response) => {
 
     const { email, password } = req.body;
-   
+
     res.status(200).json({
         ok: true,
         msg: 'login',
