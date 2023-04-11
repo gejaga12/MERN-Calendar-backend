23126e1d97951117234302ec94bf3aef1424bc2d
gejaga12 <gejaga12@hotmail.com>
2023-04-11 10:44:57 -0300

    backend terminado

--- controllers/events.js
+++ controllers/events.js
@@ -41,7 +41,7 @@ const actualizarEvento = async (req, res = response) => {
             const evento = await Evento.findById(eventoId);
 
             if (!evento) {
-                  res.status(404).json({
+                  return res.status(404).json({
                         ok: false,
                         msg: "Evento no existe por ese ID"
                   })
@@ -76,12 +76,42 @@ const actualizarEvento = async (req, res = response) => {
       }
 }
 
-const eliminarEvento = (req, res = response) => {
-      res.json({
-            ok: true,
-            msg: 'ELIMINAR EVENTOS',
+const eliminarEvento = async (req, res = response) => {
 
-      })
+      const eventoId = req.params.id;
+      const uid = req.uid;
+
+      try {
+
+            const evento = await Evento.findById(eventoId);
+
+            if (!evento) {
+                  return res.status(404).json({
+                        ok: false,
+                        msg: "Evento no existe por ese ID"
+                  })
+            }
+
+            if (evento.user.toString() !== uid) {
+                  return res.status(401).json({
+                        ok: false,
+                        msg: "No tiene privilegio de eliminar este evento"
+                  })
+            }
+
+            await Evento.findByIdAndDelete(eventoId)
+
+            res.json({
+                  ok: true,
+            })
+
+      } catch (error) {
+            console.log(error);
+            res.status(500).json({
+                  ok: false,
+                  msg: "Hable con el Administrador..."
+            })
+      }
 }
 
 module.exports = {
